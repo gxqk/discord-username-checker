2e913e3784682bd78adbc0d85f7cdaf11f4373e1
gxqk <piery250374@gmail.com>
2024-12-15 22:13:46 +0100

    Add files via upload

---
+++ DiscordUsernameChecker/DiscordUsernameChecker/Settings.cs
@@ -0,0 +1,15 @@
+﻿public class Settings
+{
+    /* SAVED CONFIG VALUES */
+    public SharpConfig.Configuration config;
+    public int Threads;
+    public bool Debug;
+    /* END SAVED CONFIG VALUES */
+
+    public Settings(string file)
+    {
+        SharpConfig.Configuration config = SharpConfig.Configuration.LoadFromFile(file);
+        Threads = config["AppSettings"]["Threads"].IntValue;
+        Debug = config["AppSettings"]["Debug"].BoolValue;
+    }
+}
